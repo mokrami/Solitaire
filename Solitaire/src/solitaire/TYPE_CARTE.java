ec047d0644e2dce9ad7baccbc03d8f8d9ec9d87d
im678382 <im678382@zola.iem>
2020-02-11 09:26:12 +0100

    premier commit

---
+++ Solitaire/src/solitaire/TYPE_CARTE.java
@@ -0,0 +1,21 @@
+package solitaire;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author im678382
+ */
+public enum TYPE_CARTE {
+    PIQUE,
+    TREFLE,
+    CARREAU,
+    COEUR,
+    JOKER_N,
+    JOKER_R
+    
+}
