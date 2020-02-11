7c1bd27fc312436571bad7f6ee9e4137427b91a2
alexis.guyot39 <alexis.guyot39@gmail.com>
2020-02-11 09:39:45 +0100

    Ajout des fonctions de paquet.

--- Solitaire/src/solitaire/Paquet.java
+++ Solitaire/src/solitaire/Paquet.java
@@ -17,4 +17,68 @@ public class Paquet {
     public Paquet(){
         this.cartes = new ArrayList<>();
     }
+    
+    public Paquet(Paquet p1, Paquet p2){
+        
+    }
+    
+    public Paquet(Paquet p, int indiceDebut, int indiceFin){
+        
+    }
+    
+    public ArrayList<Carte> getPaquet(){
+        return null;
+    }
+    
+    public Carte lireCarte(int indice){
+        return null;
+    }
+    
+    public Carte derniereCarte(){
+        return null;
+    }
+    
+    public String creerMasqueJetable(int taille){
+        return "";
+    }
+    
+    private void melange(){
+        
+    }
+    
+    private char lectureLettreAleatoire(){
+        return 'a';
+    }
+    
+    private void reculJokerNoir(){
+        
+    }
+    
+    private void reculJokerRouge(){
+        
+    }
+    
+    private void doubleCoupe(){
+        
+    }
+    
+    private void coupeSimple(){
+        
+    }
+    
+    private void reculer(int indice, int pas){
+        
+    }
+    
+    private void avance(int indice, int pas){
+        
+    }
+    
+    private void findJoker(String couleur){
+        
+    }
+    
+    private void permuter(int indCarte1, int indCarte2){
+        
+    }
 }
