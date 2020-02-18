e26bb00490613723b3ac9aef0bb7151478ba6321
im678382 <im678382@zola.iem>
2020-02-18 10:02:02 +0100

    Crypter

--- Solitaire/src/solitaire/Solitaire.java
+++ Solitaire/src/solitaire/Solitaire.java
@@ -17,7 +17,16 @@ public class Solitaire {
     public static void main(String[] args) {
         Paquet paquet;
         paquet =  new Paquet();
-        System.out.println(paquet.creerMasqueJetable(5));
+        //System.out.println(paquet.creerMasqueJetable(5));
+        Utilisateur utilisateur1 = new Utilisateur();
+        Utilisateur utilisateur2 = new Utilisateur();
+        utilisateur1.setPaquet(paquet);
+        utilisateur2.setPaquet(paquet);
+        String message = utilisateur1.crypter("bonjour");
+        System.out.println(message);
+        System.out.println("*==========================*");
+        message = utilisateur2.decrypter(message);
+        System.out.println(message);
     }
     
 }
--- Solitaire/src/solitaire/Utilisateur.java
+++ Solitaire/src/solitaire/Utilisateur.java
@@ -11,11 +11,53 @@ package solitaire;
  */
 public class Utilisateur {
     
+    private Paquet paquet;
+    
+    /**
+     * Crypte le message donné par l'utilisateur
+     * @param message message a crypter
+     * @return message crypté
+     */
     public String crypter(String message) {
-        return "";
+        String message_resultat = "";
+        String masque_jetable = this.getPaquet().creerMasqueJetable(message.length());
+        int valeurChar = 0;
+        for(int i=0; i<message.length(); i++) {
+            System.out.println(message.toUpperCase().charAt(i) + " : " + (message.toUpperCase().charAt(i) - 65));
+            System.out.println(masque_jetable.charAt(i) + " : " + (masque_jetable.charAt(i) - 65));
+            valeurChar = (message.toUpperCase().charAt(i) - 65) + (masque_jetable.charAt(i) - 65);
+            System.out.println("VALEUR : " + valeurChar);
+            if(valeurChar < 26) message_resultat+=(char)(valeurChar + 65);
+            else message_resultat+=(char)((valeurChar -26) + 65);
+        }
+        return message_resultat;
     }
     
+    /**
+     * Décrypte le message reçu par l'utilisateur
+     * @param message message a decrypter
+     * @return message décrypté
+     */
     public String decrypter(String message) {
-        return "";
+        String message_resultat = "";
+        String masque_jetable = this.getPaquet().creerMasqueJetable(message.length());
+        int valeurChar = 0;
+        for(int i=0; i<message.length(); i++) {
+            System.out.println(message.toUpperCase().charAt(i) + " : " + (message.toUpperCase().charAt(i) - 65));
+            System.out.println(masque_jetable.charAt(i) + " : " + (masque_jetable.charAt(i) - 65));
+            valeurChar = (message.toUpperCase().charAt(i) - 65) - (masque_jetable.charAt(i) - 65);
+            System.out.println("VALEUR : " + valeurChar);
+            if(valeurChar < 26) message_resultat+=(char)(valeurChar + 65);
+            else message_resultat+=(char)((valeurChar -26) + 65);
+        }
+        return message_resultat;
+    }
+    
+    public Paquet getPaquet() {
+        return this.paquet;
+    }
+    
+    public void setPaquet(Paquet p) {
+        this.paquet = p;
     }
 }
